64a7dc7a5497bbaaf62d51d3b581a967cc0f2109
LucasEzequielAcosta <48899385+LucasEzequielAcosta@users.noreply.github.com>
2019-09-30 13:00:28 -0300

    Add files via upload

--- Trabajo Practico 3/Punto8,9y10/main.c
+++ Trabajo Practico 3/Punto8,9y10/main.c
@@ -8,6 +8,13 @@ int main()
     cargarDatos();
     mostrarDatos();
 
+    abreYmuestraMenor(archivo);
+
+    iniciaMostrarInvertido(archivo);
+    iniciaInvierteArchivo(archivo);
+
+    mostrarDatos();
+
     return 0;
 }
 
@@ -43,25 +50,180 @@ void mostrarDatos()
     {
         printf("[%i]-",i);
     }
-    printf("\n");
+    fclose(archi);
 }
 
-int buscarMenorArchivoRec(int i, int menor)
+int menorEnArchivo (FILE * arch)
 {
 
-    int temporal=0;
-    FILE *archi=fopen(archivo, "rb");
-    fseek(archi,sizeof(int)*(i),SEEK_SET);
+    int menor;
+    int aux;
 
-    if(fread(&temporal,sizeof(int),1,archi)>0)
+    if(fread(&aux,sizeof(int),1,arch)==0) /// cuando llego al final del archivo
     {
-        if (temporal < menor )
-        {
-            menor = temporal;
-        }
-        menor=buscarMenorArchivoRec(i+1,menor);
+        /// me muevo uno para atras
+        fseek(arch, -1*sizeof(int), SEEK_END);
+        /// lo leo
+        fread(&menor, sizeof(int),1,arch); /// leo el ultimo elemento para retornarlo
     }
-    fclose(archi);
+    else
+    {
+        menor = menorEnArchivo(arch);
 
+        if(aux<menor)
+        {
+            menor=aux;
+        }
+    }
     return menor;
+}
+
+/// subprograma que inicia la conexión con el archivo y busca el menor
+void abreYmuestraMenor (char nombre[])
+{
+    FILE *arch=NULL;
+
+    arch=fopen(nombre, "rb");
+
+    if(arch!=NULL)
+    {
+        int menor= menorEnArchivo(arch);
+
+        printf("\n\nEl menor en el archivo es %d", menor);
+
+        fclose(arch);
+    }
+    else
+    {
+        printf("Paso algo muy malo...");
+    }
+}
+
+int leer(FILE * arch)
+{
+    int aux;
+    fread(&aux, sizeof(int),1,arch);
+    return aux;
+}
+
+void escribir(FILE *arch, int valor)
+{
+    fwrite(&valor, sizeof(int), 1, arch);
+}
+
+int cuentaRegistroArchivo(FILE * arch)
+{
+    int cant=0;
+
+    fseek(arch, 0, SEEK_END);
+    cant=ftell(arch)/sizeof(int);
+
+    return cant;
+}
+
+void invierteArchivo(FILE *arch, int i, int u)
+{
+    /// la condicion de corte con su solucion final no queda explicita
+    /// pero la idea es: contar la cantidad de registros en el archivo
+    /// e ir leyendo, intercambiando y escribiendo desde el ppio hacia el final y
+    /// viceversa
+    int ppio;
+    int fin;
+    if(i<u)
+    {
+        /// posiciono el cursor en el extremo inferior del archivo
+        fseek(arch,i*sizeof(int),SEEK_SET);
+        /// leemos el primer dato
+        ppio=leer(arch);
+
+        /// ahora en el extremo superior del archivo
+        fseek(arch, u*sizeof(int),SEEK_SET);
+        /// ahora el segundo
+        fin=leer(arch);
+
+        /// y ahora hacemos el intercambio
+
+        /// posiciono el cursor en el extremo inferior del archivo
+        fseek(arch,i*sizeof(int),SEEK_SET);
+        /// escribo el dato del final en el ppio
+        escribir(arch, fin);
+
+        /// ahora en el extremo superior del archivo
+        fseek(arch, u*sizeof(int),SEEK_SET);
+        /// y el del ppio en el final
+        escribir(arch, ppio);
+
+        /// ahora hacemos la llamada recursiva
+        /// con la aproximacion a la condicion de corte
+        invierteArchivo(arch, i+1, u-1);
+    }
+}
+
+/// otra version
+void invertir_archivo(FILE* archi)
+{
+    int dato;
+
+
+    if(fread(&dato,sizeof(int),1,archi)>0)
+    {
+
+        invertir_archivo(archi);
+
+        fwrite(&dato,sizeof(int),1,archi);
+    }
+    else
+    {
+        fseek(archi,0,SEEK_SET);
+    }
+}
+
+void iniciaInvierteArchivo(char nombre[])
+{
+    FILE *arch=NULL;
+
+    arch=fopen(nombre, "r+b");
+
+    if(arch!=NULL)
+    {
+        int cantidad=cuentaRegistroArchivo(arch);
+
+        invierteArchivo(arch, 0, cantidad-1);
+
+        fclose(arch);
+    }
+    else
+    {
+        printf("Paso algo muy malo...");
+    }
+}
+
+void muestraArchivoInvertido (FILE * arch)
+{
+    int valor;
+    // no queda explicita la condicion de corte con su solucion final...
+    if(fread(&valor, sizeof(int),1,arch)>0)
+    {
+        muestraArchivoInvertido(arch);
+        // y muestro el elemento cuando vuelve de la recursion..
+        printf(" %d", valor);
+    }
+}
+
+void iniciaMostrarInvertido(char nombre[])
+{
+    FILE *arch=NULL;
+
+    arch=fopen(nombre, "rb");
+
+    if(arch!=NULL)
+    {
+        printf("\n\nContenido del archivo invertido: ");
+        muestraArchivoInvertido(arch);
+        fclose(arch);
+    }
+    else
+    {
+        printf("Paso algo muy malo...");
+    }
 }
